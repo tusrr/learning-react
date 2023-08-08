c1fc4f867bd27e873f60097b6c4adadbc6e3bf66
tusrr <tushar.raj2000@gmail.com>
2023-08-08 12:28:49 +0530

    Age Predictor using Agify Api & React.js

--- src/App.js
+++ src/App.js
@@ -4,29 +4,27 @@ import Axios from 'axios';
 
 function App() {
 
-  // fetch("https://catfact.ninja/fact")
-  // .then((res)=>res.json())
-  // .then((data)=>{
-  //   console.log(data);
-  // });
-
-  const [catFact,setCatFact]= useState("");
-
-  const fetchCatFact=()=>
-  Axios.get("https://catfact.ninja/fact").then((res)=>{
-  setCatFact(res.data.fact);
-  })
-
- useEffect(()=>{
-      fetchCatFact();
- },[]);
-
+   const [name,SetName]= useState("Tushar");
+   const [predictedAge,SetPredictedAge]= useState(0);
+  const fetchData =  ()=>{
+      Axios.get(`https://api.agify.io/?name=${name}`).then((res)=>{
+        SetPredictedAge(res.data);
+      })
+      
+  }
   return (
     <div className="App">
-      <button onClick={fetchCatFact}>Generate Cat Fact</button>
-      <p>{catFact}</p>
+      <input 
+      placeholder='Tushar..' 
+      onChange={(event)=>{
+        SetName(event.target.value);
+      }}
+      />
+      <button onClick={fetchData}>Predict Age</button>
+      <h1> Name:{predictedAge.name} </h1>
+      <h1> Age:{predictedAge.age} </h1>
+      <h1> Your Name Occurences in this API:{predictedAge.count} </h1>
     </div>
   );
 }
-
 export default App;
