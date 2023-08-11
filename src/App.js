44763601d64317ae6f197c1339a4324c178a122b
tusrr <tushar.raj2000@gmail.com>
2023-08-12 00:31:22 +0530

    Forms in React using React-hook form & Yup

--- src/App.js
+++ src/App.js
@@ -1,47 +1,17 @@
 import './App.css';
-import {BrowserRouter as Router, Routes,Route} from 'react-router-dom';
-import { Home } from './pages/Home';
-import { Profile } from './pages/Profile';
-import { Contact } from './pages/Contact';
-import { Navbar } from './Navbar';
-import { useState,createContext  } from 'react';
-import { QueryClient,QueryClientProvider } from '@tanstack/react-query';
-export const AppContext=createContext(); 
+import { Form } from './components/Form';
+
 
 
 
 function App() {
 
-  const client = new QueryClient({
-    defaultOptions:{
-      queries:{
-        refetchOnWindowFocus:false, 
-      }
-    }
-  });
-  const [userName,setUserName]= useState("Tushar Raj");
-
   return (
     <div className="App">
-    <QueryClientProvider client={client}>
-
-    <AppContext.Provider value={{userName,setUserName}}>
-
-      <Router>
-         <Navbar/>
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/profile" element={<Profile />}/>
-          <Route path="/contact" element={<Contact/>}/>
-          <Route path="*" element={<h1>PAGE NOT FOUND</h1>}/>
-        </Routes>
-      </Router>
-
-   </AppContext.Provider>
-   </QueryClientProvider>
- 
+      <Form />
     </div>
-  );
+
+)
 }
 
 export default App;
